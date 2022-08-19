3f4727c39327308cfd1baae42a9666f90279f84e
Mihrican Pürbudak <mihricanpurbudak5@gmail.com>
2022-08-19 13:31:36 +0200

    ilk version

---
+++ src/kisiselKullanim/GithupKullanimi3.java
@@ -0,0 +1,7 @@
+package kisiselKullanim;
+
+public class GithupKullanimi3 {
+    public static void main(String[] args) {
+        System.out.println("Fatma hocayla githup ogrenmek cok zevkli");
+    }
+}
