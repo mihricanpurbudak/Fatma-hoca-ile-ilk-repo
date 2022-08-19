8cc36c0ac588c839a7038a794e2817e7f05fbfe6
Mihrican Pürbudak <mihricanpurbudak5@gmail.com>
2022-08-19 13:42:05 +0200

    ikinci version

--- src/kisiselKullanim/GithupKullanimi3.java
+++ src/kisiselKullanim/GithupKullanimi3.java
@@ -3,5 +3,7 @@ package kisiselKullanim;
 public class GithupKullanimi3 {
     public static void main(String[] args) {
         System.out.println("Fatma hocayla githup ogrenmek cok zevkli");
+        System.out.println("Fatma 1.deneme");
+
     }
 }
